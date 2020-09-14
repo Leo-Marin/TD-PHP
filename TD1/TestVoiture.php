37f373b9dd731fa03326887a86e0c800909bfeae
Leo-Marin <leo.marin@etu.umontpellier.fr>
2020-09-14 23:20:06 +0200

    TD1 5.8.5

---
+++ TD1/TestVoiture.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8" />
+        <title> Mon premier php </title>
+    </head>
+   
+    <body>
+        
+        <?php
+
+
+
+
+
+
+
+
+        ?>
+    </body>
+</html> 
