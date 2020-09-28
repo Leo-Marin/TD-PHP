9b118159dcf818d7b045ff95507a9749c12906de
Leo-Marin <leo.marin@etu.umontpellier.fr>
2020-09-28 11:26:49 +0200

    TD2 2.9.FIN bordel de marde

--- TD2/lireVoiture.php
+++ TD2/lireVoiture.php
@@ -35,9 +35,5 @@ foreach (Voiture::getAllVoitures() as $value) {
 
 
 
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 ?>
--- TD2/testConf.php
+++ TD2/testConf.php
@@ -1,9 +1,8 @@
 <?php
-  // On inclut les fichiers de classe PHP avec require_once
-  // pour éviter qu'ils soient inclus plusieurs fois
+
   require_once 'Conf.php';
 
-  // On affiche le login de la base de donnees
+
   echo Conf::getLogin() . "</br>";
   echo Conf::getHostname() . "</br>";
   echo Conf::getDatabase() . "</br>";
