c0d6aa551a4a3a9fb9df39d555b286a5ff142d90
Leo-Marin <leo.marin@etu.umontpellier.fr>
2020-10-12 13:30:34 +0200

    TDD 2.1.6.5.0

--- TD4/controller/ControllerVoiture.php
+++ TD4/controller/ControllerVoiture.php
@@ -1,15 +1,24 @@
 <?php
+
 require_once ('../model/ModelVoiture.php'); // chargement du modèle
+
 class ControllerVoiture {
+
     public static function readAll() {
         $tab_v = ModelVoiture::getAllVoitures();     //appel au modèle pour gerer la BD
         require ('../view/voiture/list.php');  //"redirige" vers la vue
     }
+
     public static function read() {
         $immat = $_GET['immatriculation'];
         $v = ModelVoiture::getVoitureByImmat($immat);
-        require ('../view/voiture/detail.php');
-       
+        if ($v == null) {
+            require '../view/voiture/error.php';
+        } else {
+            require ('../view/voiture/detail.php');
+        }
     }
+
 }
+
 ?>
---
+++ TD4/view/voiture/error.php
@@ -0,0 +1,14 @@
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Liste des voitures</title>
+    </head>
+    <body>
+        <?php
+        echo "Aucune voiture ne correspond à votre immatriculation";
+        ?>
+
+    </body>
+</html>
